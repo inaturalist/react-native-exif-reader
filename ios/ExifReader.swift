9170d8d73ee1fc054e0f67f698be03129b04d6dd
Amanda Bullington <albullington@gmail.com>
2024-09-06 18:03:41 -0700

    fix: pass time zone offset correctly

--- ios/ExifReader.swift
+++ ios/ExifReader.swift
@@ -16,6 +16,7 @@ class ExifReader: NSObject {
                 let df = DateFormatter()
                 df.calendar = Calendar(identifier: .gregorian)
                 df.dateFormat = "yyyy:MM:dd HH:mm:ss"
+                var timeDiff: Double = 0
 
                 // sometimes different fields are populated, based on how & where the
                 // photo was digitized.
@@ -28,12 +29,35 @@ class ExifReader: NSObject {
                     tzOffset = tzOffsetExif
                 }
 
+                if let tzOffset = tzOffset {
+                    let tzDateFormatter = DateFormatter()
+                    tzDateFormatter.dateFormat = "ZZZZZ"
+
+                    if let tzDate = tzDateFormatter.date(from: tzOffset),
+                       let gmtDate = tzDateFormatter.date(from: "+00:00")
+                    {
+                        if tzOffset.hasPrefix("-") {
+                            timeDiff = tzDate.timeIntervalSince(gmtDate) * -1
+                        } else {
+                            timeDiff = gmtDate.timeIntervalSince(tzDate)
+                        }
+
+                        if let tz = TimeZone(secondsFromGMT: Int(timeDiff)) {
+                            df.timeZone = tz
+                        }
+                    }
+                }
+
+
                 if let takenDateExif = exif["DateTimeOriginal"] as? String,
                    let takenDate = df.date(from: takenDateExif)
                 {
                     let formatter = DateFormatter()
                     formatter.calendar = Calendar(identifier: .gregorian)
                     formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+                    if let tz = TimeZone(secondsFromGMT: Int(timeDiff)) {
+                        formatter.timeZone = tz
+                    }
                     response["date"] = formatter.string(from:takenDate)
                     response["timezone_offset"] = tzOffset
                 }
